628b85df4c6baa18120264da855cef3c718e1957
Bojie Jiang <jiangbojie@gmail.com>
2018-04-06 16:50:22 -0700

    import

---
+++ ch7/import.py
@@ -0,0 +1,21 @@
+# # import ch7.p_for
+# import ch7.p_for as m
+#
+# # print(ch7.p_for.b)
+# print(m.b)
+
+# from ch7.p_for import b
+#
+# print(b)
+
+from ch7.t.c7 import *
+
+print(a)
+print(c)
+
+
+
+
+
+
+
